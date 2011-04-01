f4ab5668496cad3cb418b9acbc1c10fecdfb17ff
Karen Knight <karenlouise_co@comcast.net>
2011-03-31 22:06:13 -0600

    Added controller and view for Donate

---
+++ src/LifebyteMVC3/Lifebyte.Web/Controllers/DonateController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Lifebyte.Web.Controllers
+{
+    public class DonateController : Controller
+    {
+        /// <summary>
+        /// Page for Donating to lifebyte
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult index()
+        {
+            return View();
+        }
+    }
+}
