b9608417acc1e1d9db6a1fb63b20215c3fcdaaff
Jon Lindell <jhlindell@yahoo.com>
2011-03-31 22:04:13 -0600

    Added Volunteer Controller

---
+++ src/LifebyteMVC3/Lifebyte.Web/Controllers/VolunteerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Lifebyte.Web.Controllers
+{
+    public class VolunteerController: Controller
+    {
+        /// <summary>
+        /// This page is for coordinating volunteer efforts
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
