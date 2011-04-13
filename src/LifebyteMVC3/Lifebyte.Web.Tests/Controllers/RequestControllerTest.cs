6aa4a5856f14e0277245abb6d8d88576b8f94a44
Sruthi Kumar <sruthi.kumar@hotmail.com>
2011-04-12 22:08:01 -0600

    Added Request page

---
+++ src/LifebyteMVC3/Lifebyte.Web.Tests/Controllers/RequestControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Lifebyte.Web.Controllers;
+using NUnit.Framework;
+
+namespace Lifebyte.Web.Tests.Controllers
+{
+    [TestFixture]
+    public class RequestControllerTest
+    {
+        [Test]
+        public void RequestController_Index_loads_View()
+        {
+            var controller = new RequestController();
+            ActionResult result = controller.Index();
+            Assert.IsInstanceOf<ViewResult>(result);
+        }
+    }
+}
