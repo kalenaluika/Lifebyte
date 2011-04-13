d69972cd71f2e85469f9ca43c6ac74300e67567e
Dan Stewart <dan@stewshack.com>
2011-04-12 21:11:36 -0600

    Changed MSTest project to NUnit project so that Express Editions can do TDD

---
+++ src/LifebyteMVC3/Lifebyte.Web.Tests/Controllers/DonateControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using Lifebyte.Web.Controllers;
+using NUnit.Framework;
+
+namespace Lifebyte.Web.Tests.Controllers
+{
+    [TestFixture]
+    public class DonateControllerTest
+    {
+        /// <summary>
+        /// Tests the Donate/Index route result.
+        /// </summary>
+        [Test]
+        public void DonateController_Index_GoToPage()
+        {
+            var controller = new DonateController();
+            ActionResult result = controller.Index();
+
+            Assert.IsInstanceOf(typeof(ViewResult), result);
+        }
+    }
+}
---
+++ src/LifebyteMVC3/Lifebyte.Web.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using Lifebyte.Web.Controllers;
+using NUnit.Framework;
+
+namespace Lifebyte.Web.Tests.Controllers
+{
+[TestFixture]
+    public class HomeControllerTest
+    {
+        /// <summary>
+        /// This is a test of the Index action on the HomeController.
+        /// </summary>
+        [Test]
+        public void HomeController_Index_ReturnsView()
+        {
+            var controller = new HomeController();
+            ActionResult result = controller.Index();
+
+            Assert.IsInstanceOf(typeof(ViewResult), result);
+        }
+    }
+}
