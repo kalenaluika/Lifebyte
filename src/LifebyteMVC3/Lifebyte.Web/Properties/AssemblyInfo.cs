aa8569ecab1350a604f13f6d05c46d811023246d
Jon Lindell <jhlindell@yahoo.com>
2011-04-26 22:27:53 -0600

    got basic log4net stuff working

--- src/LifebyteMVC3/Lifebyte.Web/Properties/AssemblyInfo.cs
+++ src/LifebyteMVC3/Lifebyte.Web/Properties/AssemblyInfo.cs
@@ -33,6 +33,3 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-// Log4net
-[assembly: log4net.Config.XmlConfigurator(ConfigFile = "Log4Net.config", Watch = true)]
